ff71d1bb282924c025bad47f0cb4e20ddbada709
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-10-25 19:58:34 +0530

    integration of frontal face detection

--- router/recognize.py
+++ router/recognize.py
@@ -1,13 +1,19 @@
 from flask import request
+from scripts import haarcascade
 import base64 
 
 def index():
-    title = request.get_json()["title"]
+    #accept the data from the request
+    title = request.get_json()["title"] 
     b64 = request.get_json()["video"]
     
+    #convert the base64 video to a file and save it locally
     video = base64.b64decode(b64)
-    video_result = open('res.mp4', 'wb') # create a writable image and write the decoding result
+    video_result = open('res.mp4', 'wb')
     video_result.write(video)
+    print("Video is saved in the local")
     
-    return "Video conversion is done"
-    # return "The video's title is {}".format(title)
+    #run the face recognition algorithm on the video
+    haarcascade.index("res.mp4")
+    
+    return "Broke the video into frames and saved the faces from each frames."
---
+++ scripts/haarcascade.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+
+def index(vidPath):
+    video = cv2.VideoCapture(vidPath)
+    length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(length)
+    i=0
+    while True:
+        i+=1
+        # Capture frame-by-frame
+        ret, frame = video.read()
+        if ret == False:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.3,
+            minNeighbors=3,
+            minSize=(30, 30)
+        )
+        
+        print("[INFO] Found {0} Faces.".format(len(faces)))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi_color = frame[y:y + h, x:x + w]
+            print("[INFO] Object found. Saving locally.")
+            cv2.imwrite('frame_' + str(i) + '_' + str(w) + str(h) + '_faces.jpg', roi_color)
+
+        status = cv2.imwrite('faces_detected.jpg', frame)
+        print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
