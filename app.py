b6a2aa8469efb39584933ffcded9d22b35387fed
Piyush <piyushb544@gmail.com>
2022-10-27 13:17:30 +0530

    trying to communicate the servers

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from router import recognize, location
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 app.add_url_rule('/recognize', 'recognize', recognize.index, methods=["POST"])
 app.add_url_rule('/location', 'location', location.index, methods=["POST"])
 
