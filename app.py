cd726bc0c5096a8b0b36af57e479bc32fcf787b4
Piyush <piyushb544@gmail.com>
2022-10-28 12:21:42 +0530

    static templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from router import recognize, location
 from flask_cors import CORS
@@ -7,6 +7,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('video_face_reco.html')
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
--- router/recognize.py
+++ router/recognize.py
@@ -1,20 +1,24 @@
-from flask import request
+from __future__ import print_function
+from flask import Flask, jsonify, request, render_template
 from scripts import haarcascade
 import base64 
 
 def index():
+    if request.method == "POST":
+        print("Recieved anything?")
     #accept the data from the request
-    title = request.get_json()["title"] 
-    b64 = request.get_json()["video"]
-    location = request.get_json()["location"]
-    
-    #convert the base64 video to a file and save it locally
-    video = base64.b64decode(b64)
-    video_result = open('res.mp4', 'wb')
-    video_result.write(video)
-    print("Video is saved in the local")
-    
-    #run the face recognition algorithm on the video
-    haarcascade.index("res.mp4", location)
-    
+        title = request.form.get('title') 
+        location = request.form.get('location') 
+        # b64 = request.get_json()["video"]
+        # location = request.get_json()["location"]
+        print(location,title)
+        #convert the base64 video to a file and save it locally
+        # video = base64.b64decode(b64)
+        # video_result = open('res.mp4', 'wb')
+        # video_result.write(video)
+        # print("Video is saved in the local")
+        
+        # #run the face recognition algorithm on the video
+        # haarcascade.index("res.mp4", location)
+        
     return "Broke the video into frames and saved the faces from each frames."
