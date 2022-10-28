098271499b56c4cad6949ff1a53b69971af6ae6e
Piyush <piyushb544@gmail.com>
2022-10-28 08:26:37 +0530

    cors allowed

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 from router import recognize, location
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
 app.add_url_rule('/recognize', 'recognize', recognize.index, methods=["POST"])
 app.add_url_rule('/location', 'location', location.index, methods=["POST"])
 
